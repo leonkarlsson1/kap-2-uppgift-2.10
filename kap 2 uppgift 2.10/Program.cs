a903a76aa2956e199c24724de63bad240ba54169
Alexsept177 <alka04006@utb.vaxjo.se>
2022-09-07 11:57:10 +0200

    Add project files.

---
+++ kap 2 uppgift 2.10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ 
+namespace Uppgift_2._10
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vad är din favorti bok?");
+            string HarryPotter=Console.ReadLine();
+            Console.WriteLine("HarryPotter");
+        }
+    }
+        
+}
